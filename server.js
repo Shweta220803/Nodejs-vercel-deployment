86ad503f8e42501b26f441acd6838b3ab884826d
Shweta220803 <shwetaguraru99@gmail.com>
2025-04-19 06:26:41 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API HOME IS WORKING");
+});
+app.get("/contact", (req, res) => {
+  res.send("API CONTACT IS WORKING");
+});
+
+// const PORT = 5001;
+// app.listen(PORT, () => {
+//   console.log(`Server is running at PORT ${PORT}`);
+// });
+console.log("Server is running");
+
+export default app;
